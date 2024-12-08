cea3e774cef8209ca95bcd92f851200223aa7311
tznind <tznind@dundee.ac.uk>
2024-12-08 20:33:17 +0000

    WIP WindowsConsole / WindowsOutput

--- Terminal.Gui/ConsoleDrivers/WindowsDriver/WindowsConsole.cs
+++ Terminal.Gui/ConsoleDrivers/WindowsDriver/WindowsConsole.cs
@@ -212,15 +212,15 @@ public class WindowsConsole
 
             // TODO: requires extensive testing if we go down this route
             // If console output has changed
-            if (s != _lastWrite)
-            {
+            //if (s != _lastWrite)
+            //{
                 // supply console with the new content
                 result = WriteConsole (_outputHandle, s, (uint)s.Length, out uint _, nint.Zero);
                 
                 
                 // Uncomment this to see that it does render properly :/
                 // Process.GetCurrentProcess ().Kill();
-            }
+            //}
 
             _lastWrite = s;
 
