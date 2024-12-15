c01f974edda6ce98ca5b0eedc69ea219fc7d169e
tznind <tznind@dundee.ac.uk>
2024-12-15 18:25:22 +0000

    Create first test for Application2

---
+++ UnitTests/ConsoleDrivers/V2/ApplicationV2Tests.cs
@@ -0,0 +1,19 @@
+﻿using Terminal.Gui.ConsoleDrivers.V2;
+
+namespace UnitTests.ConsoleDrivers.V2;
+public class ApplicationV2Tests
+{
+    [Fact]
+    public void TestInit_CreatesKeybindings ()
+    {
+        var v2 = new ApplicationV2 ();
+
+        Application.KeyBindings.Clear();
+
+        Assert.Empty(Application.KeyBindings.GetBindings ());
+
+        v2.Init ();
+
+        Assert.NotEmpty (Application.KeyBindings.GetBindings ());
+    }
+}
