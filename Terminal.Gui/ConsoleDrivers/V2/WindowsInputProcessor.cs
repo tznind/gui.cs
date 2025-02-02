8689fe944c71c5e62e3568fdc98e6631f9330f7e
tznind <tznind@dundee.ac.uk>
2025-02-02 17:33:12 +0000

    Fix WindowsInputProcessor to use the full key mapping code from WindowsDriver

--- Terminal.Gui/ConsoleDrivers/V2/WindowsInputProcessor.cs
+++ Terminal.Gui/ConsoleDrivers/V2/WindowsInputProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿#nullable enable
+using System.Collections.Concurrent;
 using static Terminal.Gui.ConsoleDrivers.ConsoleKeyMapping;
 using static Terminal.Gui.WindowsConsole;
 
@@ -68,52 +69,38 @@ internal class WindowsInputProcessor : InputProcessor<InputRecord>
     /// <inheritdoc/>
     protected override void ProcessAfterParsing (InputRecord input)
     {
-        Key key;
-
-        if (input.KeyEvent.UnicodeChar == '\0')
+        // TODO: This should be in a shared helper method not calling statics in WindowsDriver
+        if(InputRecordToKey (input, out var key))
         {
-            key = input.KeyEvent.wVirtualKeyCode switch
-                  {
-                      VK.DOWN => Key.CursorDown,
-                      VK.UP => Key.CursorUp,
-                      VK.LEFT => Key.CursorLeft,
-                      VK.RIGHT => Key.CursorRight,
-                      VK.BACK => Key.Backspace,
-                      VK.TAB => Key.Tab,
-                      VK.F1 => Key.F1,
-                      VK.F2 => Key.F2,
-                      VK.F3 => Key.F3,
-                      VK.F4 => Key.F4,
-                      VK.F5 => Key.F5,
-                      VK.F6 => Key.F6,
-                      VK.F7 => Key.F7,
-                      VK.F8 => Key.F8,
-                      VK.F9 => Key.F9,
-                      VK.F10 => Key.F10,
-                      VK.F11 => Key.F11,
-                      VK.F12 => Key.F12,
-                      VK.F13 => Key.F13,
-                      VK.F14 => Key.F14,
-                      VK.F15 => Key.F15,
-                      VK.F16 => Key.F16,
-                      VK.F17 => Key.F17,
-                      VK.F18 => Key.F18,
-                      VK.F19 => Key.F19,
-                      VK.F20 => Key.F20,
-                      VK.F21 => Key.F21,
-                      VK.F22 => Key.F22,
-                      VK.F23 => Key.F23,
-                      VK.F24 => Key.F24,
-                      _ => '\0'
-                  };
+            OnKeyDown (key!);
+            OnKeyUp (key!);
         }
-        else
+    }
+
+    private bool InputRecordToKey (InputRecord inputEvent, out Key? key)
+    {
+        if (inputEvent.KeyEvent.wVirtualKeyCode == (VK)ConsoleKey.Packet)
         {
-            key = input.KeyEvent.UnicodeChar;
+            // Used to pass Unicode characters as if they were keystrokes.
+            // The VK_PACKET key is the low word of a 32-bit
+            // Virtual Key value used for non-keyboard input methods.
+            inputEvent.KeyEvent = WindowsDriver.FromVKPacketToKeyEventRecord (inputEvent.KeyEvent);
         }
 
-        OnKeyDown (key);
-        OnKeyUp (key);
+        WindowsConsole.ConsoleKeyInfoEx keyInfo = WindowsDriver.ToConsoleKeyInfoEx (inputEvent.KeyEvent);
+
+        //Debug.WriteLine ($"event: KBD: {GetKeyboardLayoutName()} {inputEvent.ToString ()} {keyInfo.ToString (keyInfo)}");
+
+        KeyCode map = WindowsDriver.MapKey (keyInfo);
+
+        if (map == KeyCode.Null)
+        {
+            key = null;
+            return false;
+        }
+
+        key = new Key (map);
+        return true;
     }
 
     private MouseEventArgs ToDriverMouse (MouseEventRecord e)
