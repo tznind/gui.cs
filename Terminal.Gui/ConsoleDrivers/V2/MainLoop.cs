5d190735479d640bb7ef50e9d226fbbe0a940a9d
tznind <tznind@dundee.ac.uk>
2025-03-02 08:59:08 +0000

    Remove iteration limit for benchmarking in v2

--- Terminal.Gui/ConsoleDrivers/V2/MainLoop.cs
+++ Terminal.Gui/ConsoleDrivers/V2/MainLoop.cs
@@ -99,11 +99,12 @@ public class MainLoop<T> : IMainLoop<T>
     public void Iteration ()
     {
         DateTime dt = Now ();
+        int timeAllowed = 1000 / Math.Max(1,(int)Application.MaximumIterationsPerSecond);
 
         IterationImpl ();
 
         TimeSpan took = Now () - dt;
-        TimeSpan sleepFor = TimeSpan.FromMilliseconds (50) - took;
+        TimeSpan sleepFor = TimeSpan.FromMilliseconds (timeAllowed) - took;
 
         Logging.TotalIterationMetric.Record (took.Milliseconds);
 
