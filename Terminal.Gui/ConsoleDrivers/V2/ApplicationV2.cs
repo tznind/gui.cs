1bc647d2daac8276d6fed3eace1c7bf83d1ba320
tznind <tznind@dundee.ac.uk>
2025-04-03 13:01:53 +0100

    Revert "Add class for detecting information about console in extensible way"
    This reverts commit 7e4253cf28428ea80a4773b137d4bd89cf321746.

--- Terminal.Gui/ConsoleDrivers/V2/ApplicationV2.cs
+++ Terminal.Gui/ConsoleDrivers/V2/ApplicationV2.cs
@@ -85,14 +85,10 @@ public class ApplicationV2 : ApplicationImpl
 
     private void CreateDriver (string? driverName)
     {
+        PlatformID p = Environment.OSVersion.Platform;
 
         bool definetlyWin = driverName?.Contains ("win") ?? false;
         bool definetlyNet = driverName?.Contains ("net") ?? false;
-
-        var finder = new ConsoleFeatureFinder ();
-        var result = finder.GetResults ();
-
-        Logging.Logger.LogInformation ($"Feature detection results:{ result}");
 
         if (definetlyWin)
         {
@@ -102,7 +98,7 @@ public class ApplicationV2 : ApplicationImpl
         {
             _coordinator = CreateNetSubcomponents ();
         }
-        else if (result.IsWindows)
+        else if (p == PlatformID.Win32NT || p == PlatformID.Win32S || p == PlatformID.Win32Windows)
         {
             _coordinator = CreateWindowsSubcomponents ();
         }
