52e1dd91a573ed2acc910e8884691db306e5000c
tznind <tznind@dundee.ac.uk>
2025-05-03 10:12:02 +0100

    Add more tests, this time for table view navigation

--- Tests/IntegrationTests/FluentTests/FileDialogFluentTests.cs
+++ Tests/IntegrationTests/FluentTests/FileDialogFluentTests.cs
@@ -273,4 +273,90 @@ public class FileDialogFluentTests
                           .WriteOutLogs (_out)
                           .Stop ();
     }
+
+    [Theory]
+    [ClassData (typeof (V2TestDrivers_WithTrueFalseParameter))]
+    public void SaveFileDialog_TableView_UpDown_PreserveFilenameOnDirectoryChanges_True (V2TestDriver d, bool preserve)
+    {
+        var sd = new SaveDialog (CreateExampleFileSystem ()) { Modal = false };
+        sd.Style.PreserveFilenameOnDirectoryChanges = preserve;
+
+        using var c = With.A (sd, 100, 20, d)
+                          .ScreenShot ("Save dialog", _out)
+                          .Then (() => Assert.True (sd.Canceled))
+                          .Focus<TextField> (_ => true)
+                          // Clear selection by pressing right in 'file path' text box
+                          .RaiseKeyDownEvent (Key.CursorRight)
+                          .AssertIsType<TextField> (sd.Focused)
+                          // Type a filename into the dialog
+                          .RaiseKeyDownEvent (Key.H)
+                          .RaiseKeyDownEvent (Key.E)
+                          .RaiseKeyDownEvent (Key.L)
+                          .RaiseKeyDownEvent (Key.L)
+                          .RaiseKeyDownEvent (Key.O)
+                          .WaitIteration ()
+                          .ScreenShot ("After typing filename 'hello'", _out)
+                          .AssertEndsWith ("hello", sd.Path)
+                          .Focus<TableView> (_ => true)
+                          .ScreenShot ("After focus table", _out)
+                          .Down ()
+                          .ScreenShot ("After down in table", _out);
+
+        if (preserve)
+        {
+            c.AssertContains ("logs", sd.Path)
+             .AssertEndsWith ("hello", sd.Path);
+        }
+        else
+        {
+            c.AssertContains ("logs", sd.Path)
+             .AssertDoesNotContain ("hello", sd.Path);
+        }
+
+        c.Up ()
+         .ScreenShot ("After up in table", _out);
+
+        if (preserve)
+        {
+            c.AssertContains ("empty-dir", sd.Path)
+             .AssertEndsWith ("hello", sd.Path);
+        }
+        else
+        {
+            c.AssertContains ("empty-dir", sd.Path)
+             .AssertDoesNotContain ("hello", sd.Path);
+        }
+
+        c.Enter ()
+         .ScreenShot ("After enter in table", _out); ;
+
+
+        if (preserve)
+        {
+            c.AssertContains ("empty-dir", sd.Path)
+             .AssertEndsWith ("hello", sd.Path);
+        }
+        else
+        {
+            c.AssertContains ("empty-dir", sd.Path)
+             .AssertDoesNotContain ("hello", sd.Path);
+        }
+
+        c.LeftClick<Button> (b => b.Text == "_Save");
+        c.AssertFalse (sd.Canceled);
+
+        if (preserve)
+        {
+            c.AssertContains ("empty-dir", sd.Path)
+             .AssertEndsWith ("hello", sd.Path);
+        }
+        else
+        {
+            c.AssertContains ("empty-dir", sd.Path)
+             .AssertDoesNotContain ("hello", sd.Path);
+        }
+
+        c.WriteOutLogs (_out)
+         .Stop ();
+    }
 }
