9f260225b3016bb8cb5ac3d925621096f84ac9bf
tznind <tznind@dundee.ac.uk>
2025-07-04 06:37:50 +0100

    Working test woo

---
+++ Terminal.Gui.Analyzers.Tests/HandledEventArgsAnalyzerTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Terminal.Gui.Analyzers.Tests;
+
+public class HandledEventArgsAnalyzerTests
+{
+    [Fact]
+    public async Task Should_ReportDiagnostic_When_EHandledNotSet ()
+    {
+        var originalCode = @"
+using Terminal.Gui.Views;
+
+class TestClass
+{
+    void Setup()
+    {
+        var b = new Button();
+        b.Accepting += (s, e) =>
+        {
+            // Forgot e.Handled = true;
+        };
+    }
+}";
+        await new ProjectBuilder ()
+              .WithSourceCode (originalCode)
+              .WithAnalyzer (new Terminal.Gui.Analyzers.HandledEventArgsAnalyzer ())
+              .ValidateAsync ();
+    }
+}
